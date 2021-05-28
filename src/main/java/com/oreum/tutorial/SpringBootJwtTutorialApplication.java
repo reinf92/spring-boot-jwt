c14bbfc15346c468406fc65c9b0535062140bc26
taeho kim <reinf92@naver.com>
2021-05-28 17:27:17 +0900

    first commit

---
+++ src/main/java/com/oreum/tutorial/SpringBootJwtTutorialApplication.java
@@ -0,0 +1,13 @@
+package com.oreum.tutorial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootJwtTutorialApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootJwtTutorialApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/oreum/tutorial/SpringBootJwtTutorialApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oreum.tutorial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootJwtTutorialApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

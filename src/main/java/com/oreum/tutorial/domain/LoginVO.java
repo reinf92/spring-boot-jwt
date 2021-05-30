87589bce8d229430fffd36a55127494e1a3e9a52
taeho kim <reinf92@naver.com>
2021-05-30 19:24:02 +0900

    Domain, Repository, Login API

---
+++ src/main/java/com/oreum/tutorial/domain/LoginVO.java
@@ -0,0 +1,22 @@
+package com.oreum.tutorial.domain;
+
+import lombok.*;
+
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class LoginVO {
+
+    @NotNull
+    @Size(min = 3, max = 50)
+    private String username;
+
+    @NotNull
+    @Size(min = 3, max = 100)
+    private String password;
+}
---
+++ src/main/java/com/oreum/tutorial/domain/TokenVO.java
@@ -0,0 +1,13 @@
+package com.oreum.tutorial.domain;
+
+import lombok.*;
+
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class TokenVO {
+
+    private String token;
+}
